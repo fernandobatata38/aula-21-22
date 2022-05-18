11b4859ee5bd96703654fdf24718a250bb776381
fernandobatata38 <101750484+fernandobatata38@users.noreply.github.com>
2022-05-18 18:49:50 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C21_matterjs-Boilerplate
+# PRO-C22-wireframe
+wire frame for c22 
--- sketch.js
+++ sketch.js
@@ -1,35 +1,72 @@
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var engine, world, ground;
+var imgbackground;
+var torre, torreimg;
+var canhao;
+var angle;
 
-let engine;
-let world;
-
-var chao, teto, pardir, paresq;
+var esferas=[];
+function preload() {
+  imgbackground = loadImage("assets/background.gif");
+  torreimg = loadImage("assets/tower.png");
+}
 
 function setup() {
-  createCanvas(400,400);
+
+  angleMode(DEGREES);
+  angle=20;
+
+  canvas = createCanvas(1200, 600);
   engine = Engine.create();
   world = engine.world;
 
-  chao = new Ground(200,395,400,10);
-  teto = new Ground(200,5,400,10);
-  pardir = new Ground(395, 200, 10, 400);
-  paresq = new Ground(5, 200, 10, 400);
+  var options = {
+    isStatic: true
+  }
 
-  rectMode(CENTER);
-  ellipseMode(RADIUS);
+  ground = Bodies.rectangle(0, height - 1, width * 2, 1, options);
+  World.add(world, ground);
+
+  torre = Bodies.rectangle(160,350,160,310,options);
+  World.add(world, torre);
+ 
+  canhao=new Cannon(180,110,130,100,angle);
   
-}
 
-function draw() 
-{
-  background(51);
+ 
+}
+function keyReleased(){
+  if(keyCode===DOWN_ARROW){
+    esferas[esferas.length-1].one_puuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuunchhh();
+  }
+}
+function draw() {
+  background(189);
   Engine.update(engine);
-  chao.show();
-  teto.show();
-  pardir.show();
-  paresq.show();
+  image(imgbackground,0,0,1200,600);
+  rect(ground.position.x, ground.position.y, width *2, 1);
+  
+  push();
+  imageMode(CENTER);
+  image(torreimg, torre.position.x, torre.position.y, 160,310);
+  pop();
+  canhao.display();
+  for(var i=0;i<esferas.length;i++){
+    la_vai_bomba(esferas[i]);
+  }
 
 }
-
+function keyPressed(){
+  if(keyCode===DOWN_ARROW){
+    var bolacanhao=new CannonBall(canhao.x,canhao.y);
+    esferas.push(bolacanhao);
+  }
+}
+function la_vai_bomba(bola){
+  if(bola){
+    bola.display();
+  }
+}
